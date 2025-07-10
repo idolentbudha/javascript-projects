67c62546658660a627286d1f6e86dbf258c4da39
Buddha Lama <buddha.lama.moktan281@gmail.com>
2025-07-10 23:17:07 +0100

    project3 math operators assignment

---
+++ basic-javascript-projects/project3_math_operators/js/main.js
@@ -0,0 +1,12 @@
+// project3_math_operator - Math Operators Assignment
+
+function displayAdditionResult() {
+  // Perform an addition operation
+  let number1 = 10;
+  let number2 = 5;
+  let result = number1 + number2;
+
+  // Display the result in the paragraph with id "Math"
+  document.getElementById("Math").innerHTML =
+    "The result of " + number1 + " +" + number2 + " is: " + result;
+}
