a6df2aa80c9d1f9146a6f71da2477b466ea690cf
Buddha Lama <buddha.lama.moktan281@gmail.com>
2025-07-11 00:42:40 +0100

    project4 dictionaries assignment

--- basic-javascript-projects/project4_dictionaries/js/main.js
+++ basic-javascript-projects/project4_dictionaries/js/main.js
@@ -6,10 +6,41 @@ function displayDictionaryValue() {
     name: "Alice",
     age: 30,
     occupation: "Engineer",
-    location: "London"
+    location: "London",
   };
 
   // Display a value from the dictionary
   document.getElementById("Dictionary").innerHTML =
     "Name: " + person.name + "<br>Occupation: " + person.occupation;
+}
+
+// Delete Assignment - Removing a property from a dictionary
+
+function deleteDictionaryValue() {
+  const car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    color: "Blue",
+  };
+  const oldCar = structuredClone(car);
+  // Delete the 'color' property
+  delete car.color;
+
+  // Output the remaining dictionary keys and values
+  document.getElementById("Dictionary__delete").innerHTML =
+    "Original car object:" +
+    JSON.stringify(oldCar) +
+    "<br><br>" +
+    "Car object after delete:" +
+    JSON.stringify(car) +
+    "<br><br>" +
+    "Brand: " +
+    car.brand +
+    "<br>Model: " +
+    car.model +
+    "<br>Year: " +
+    car.year +
+    "<br>Color: " +
+    car.color;
 }
