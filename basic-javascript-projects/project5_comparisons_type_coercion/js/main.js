bfda3654aa9fa0733a3740790b1b48accc770aa4
Buddha Lama <buddha.lama.moktan281@gmail.com>
2025-07-11 01:02:17 +0100

    Type Coercion Assignment

--- basic-javascript-projects/project5_comparisons_type_coercion/js/main.js
+++ basic-javascript-projects/project5_comparisons_type_coercion/js/main.js
@@ -3,3 +3,11 @@ function showTypeOf() {
   const resultContent = `<code>let sampleValue = 123;</code> <br> typeof sampleValue is ${typeof sampleValue}`;
   document.getElementById("typeOfResult").innerHTML = resultContent;
 }
+
+function typeCoercionExample() {
+  let number = 5;
+  let text = " apples";
+  let result = number + text; // Type coercion occurs here
+  const output = `<code>let number = 5; let text = " apples"; let result = number + text;</code><br>Result: ${result} <br>Type: ${typeof result}`;
+  document.getElementById("typeCoercionResult").innerHTML = output;
+}
