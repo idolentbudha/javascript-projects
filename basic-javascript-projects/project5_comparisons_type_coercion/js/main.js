857a4fe14594ca0fb0963d690db58936215ff9cd
Buddha Lama <buddha.lama.moktan281@gmail.com>
2025-07-11 01:08:24 +0100

    NAN Assignment

--- basic-javascript-projects/project5_comparisons_type_coercion/js/main.js
+++ basic-javascript-projects/project5_comparisons_type_coercion/js/main.js
@@ -11,3 +11,15 @@ function typeCoercionExample() {
   const output = `<code>let number = 5; let text = " apples"; let result = number + text;</code><br>Result: ${result} <br>Type: ${typeof result}`;
   document.getElementById("typeCoercionResult").innerHTML = output;
 }
+
+function showInfinityExamples() {
+  let positiveInfinity = 1.7976931348623157E+10308;
+  let negativeInfinity = -1.7976931348623157E+10308;
+
+  const output = `
+    <code>let positiveInfinity = 1.7976931348623157E+10308;</code><br>
+    <code>let negativeInfinity = -1.7976931348623157E+10308;</code><br><br>
+    Result: ${positiveInfinity}, ${negativeInfinity}
+  `;
+  document.getElementById("infinityResult").innerHTML = output;
+}
