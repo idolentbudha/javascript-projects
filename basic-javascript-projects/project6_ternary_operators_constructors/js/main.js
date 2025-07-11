07f7c151efbc568322f5b8215c5b27cd7f3746bb
Buddha Lama <buddha.lama.moktan281@gmail.com>
2025-07-11 11:24:40 +0100

    Final Project6_Ternary_Operators_Constructors Assignment

--- basic-javascript-projects/project6_ternary_operators_constructors/js/main.js
+++ basic-javascript-projects/project6_ternary_operators_constructors/js/main.js
@@ -1,13 +1,32 @@
+// Function using ternary operator based on input height
 function rideFunction() {
   let height, canRide;
   height = document.getElementById("Height").value;
+  // Ternary operation to check height requirement
   canRide = height < 52 ? "Sorry, you are too short" : "You are tall enough";
-  document.getElementById("RideResult").innerHTML = canRide + "to ride.";
+  document.getElementById("RideResult").innerHTML = canRide + " to ride.";
 }
 
+// Constructor function using 'this' and 'new'
+function Vehicle(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+}
+
+// Function to display constructor object data in the browser
+function displayVehicle() {
+  let myCar = new Vehicle("Toyota", "Corolla", 2020);
+  let description = "Make: " + myCar.make + ", Model: " + myCar.model + ", Year: " + myCar.year;
+  document.getElementById("VehicleInfo").innerHTML = description;
+}
+
+// Function containing a nested function
 function nestedFunction() {
+  // Inner function that returns a string
   function innerFunction() {
     return "This text is from a nested function!";
   }
+  // Displaying the result of the inner function
   document.getElementById("NestedFunction").innerHTML = innerFunction();
 }
