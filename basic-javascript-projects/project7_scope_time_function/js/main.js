0664dafac31d29ea7275566cf6540ec54ccf2eba
Buddha Lama <buddha.lama.moktan281@gmail.com>
2025-07-12 18:00:46 +0100

    fix issues

--- basic-javascript-projects/project7_scope_time_function/js/main.js
+++ basic-javascript-projects/project7_scope_time_function/js/main.js
@@ -1,7 +1,8 @@
+// This function demonstrates the use of local and global variables
 function scopeFunction() {
-  var globalVariable = 10;
+  var globalVariable = 10; // Global variable accessible throughout scopeFunction
   function addNumOne() {
-    var localVar = 20;
+    var localVar = 20; // Local variable accessible only inside addNumOne
     return localVar + globalVariable;
   }
   // function addNumTwo() {
@@ -13,14 +14,15 @@ function scopeFunction() {
   document.getElementById("resultMain").innerHTML = addNumOne();
 }
 
+// This function intentionally generates an error to demonstrate debugging with console.log
 function generateError() {
-  var gVar = 10;
+  var gVar = 10; // Global variable within generateError
   function addNumberSample() {
-    var localVar = 20;
+    var localVar = 20; // Local variable
     return localVar + gVar;
   }
   function addNumberTwoSample() {
-    return gVar + localVar;
+    return gVar + localVar; // Error: localVar is not defined in this scope
   }
   console.log(addNumberSample());
   console.log(addNumberTwoSample());
@@ -28,6 +30,7 @@ function generateError() {
   document.getElementById("resultMain").innerHTML = addNumberSample();
 }
 
+// This function checks the current hour and displays a message accordingly
 function checkHour() {
   var methodAssignmentResult = document.getElementById(
     "methodAssignmentResult"
@@ -40,17 +43,20 @@ function checkHour() {
       "Sorry, please try next time at some other hour.";
   }
 }
+
+// This function checks if the entered age qualifies the user to vote
 function isEligibleToVote() {
   var age = document.getElementById("Age").value;
   var ageResult_P = document.getElementById("isEligibleResult");
   if (age > 18) {
-    ageResult_P.innerHTML = "Yes your are ready to vote.";
+    ageResult_P.innerHTML = "Yes, you are ready to vote."; // Corrected typo
   } else {
     ageResult_P.innerHTML = "Sorry, you can vote only when you are old enough.";
   }
 }
 
-function generateGreetingMessage() {
+// This function displays a time-based greeting message
+function Time_function() {
   var time = new Date().getHours();
   var reply = "";
   if (time < 12 && time > 0) {
