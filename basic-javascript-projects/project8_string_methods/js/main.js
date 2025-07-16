dbf9923000781aaf73ecb5799bbf6eb4701923f6
Buddha Lama <buddha.lama.moktan281@gmail.com>
2025-07-16 22:36:13 +0100

    Project8_String_Methods Assignment

--- basic-javascript-projects/project8_string_methods/js/main.js
+++ basic-javascript-projects/project8_string_methods/js/main.js
@@ -1,3 +1,4 @@
+// Concatenates multiple strings and displays the full sentence
 function concatenateFunction() {
   var part1 = "I have ";
   var part2 = "made this ";
@@ -7,20 +8,24 @@ function concatenateFunction() {
   document.getElementById("concatenateResult").innerHTML = wholeSentence;
 }
 
+// Extracts a portion of the sentence using slice() and displays it
 function sliceMethod() {
   var sentence = "All work and no play makes Johnny a dull boy.";
-  var section = sentence.slice(27, 33);
+  var section = sentence.slice(27, 33); // Extracts 'Johnny'
   document.getElementById("SliceResult").innerHTML = section;
 }
 
+// Converts a number to a string and displays it
 function convertToString() {
   var x = 123;
-  document.getElementById("NumberToString").innerHTML = x.toString();
+  var result = `<code>var x=123; <br>var result=x.toString();<br>/*RESULT*/<br>"${x.toString()}"</code>`;
+  document.getElementById("NumberToString").innerHTML = result;
 }
 
+// Formats a number to a specified precision and displays it
 function precisionMethod() {
   var num = 123938.301239854019421124;
   var precisedNumber = num.toPrecision(10);
-  var result = `${num}.toPrecision(10) = ${precisedNumber} `;
+  var result = `${num}.toPrecision(10) = ${precisedNumber}`;
   document.getElementById("precisionResult").innerHTML = result;
 }
