08cc659499d36d7a1444a9ab6fb09a250f97d815
Buddha Lama <buddha.lama.moktan281@gmail.com>
2025-07-16 21:38:25 +0100

    slice method assignment

--- basic-javascript-projects/project8_string_methods/js/main.js
+++ basic-javascript-projects/project8_string_methods/js/main.js
@@ -1,8 +1,14 @@
 function concatenateFunction() {
-  var part1 = "I have";
+  var part1 = "I have ";
   var part2 = "made this ";
   var part3 = "into a complete ";
   var part4 = "sentence.";
   var wholeSentence = part1.concat(part2, part3, part4);
   document.getElementById("concatenateResult").innerHTML = wholeSentence;
+}
+
+function sliceMethod() {
+  var sentence = "All work and no play makes Johnny a dull boy.";
+  var section = sentence.slice(27, 33);
+  document.getElementById("SliceResult").innerHTML = section;
 }
