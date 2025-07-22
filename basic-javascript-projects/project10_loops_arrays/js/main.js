33ef61c4320c300e10657ddf7d86bacd8bce41e4
Buddha Lama <buddha.lama.moktan281@gmail.com>
2025-07-22 19:41:57 +0100

    Const Keyword Assignment

--- basic-javascript-projects/project10_loops_arrays/js/main.js
+++ basic-javascript-projects/project10_loops_arrays/js/main.js
@@ -3,3 +3,20 @@ function arrayFunction() {
   document.getElementById("Array").innerHTML =
     `${arrayOne[0]}, ${arrayOne[1]},${arrayOne[2]}`;
 }
+
+function constantFunction() {
+  const person = {
+    name: "John",
+    age: 30,
+    job: "Developer",
+  };
+
+  // Modify a property
+  person.age = 31;
+
+  // Add a new property
+  person.country = "UK";
+
+  document.getElementById("Contant").innerHTML =
+    `Name: ${person.name}, Age: ${person.age}, Job: ${person.job}, Country: ${person.country}`;
+}
