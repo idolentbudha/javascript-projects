6db439e537cd0e7463741587e7dc958f8dc43c2e
Buddha Lama <buddha.lama.moktan281@gmail.com>
2025-07-30 13:40:09 +0100

    update modal

--- one-page-site/js/script.js
+++ one-page-site/js/script.js
@@ -1,53 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const mobileMenu = document.getElementById("mobile-menu");
-  const navLinks = document.querySelector(".nav-links");
-  const navBar = document.querySelector(".navbar");
+function openModal(element) {
+  const ogImage = element.getAttribute("data-og-image");
 
-  // Toggle mobile menu
-  mobileMenu.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-    mobileMenu.classList.toggle("active"); // For animated hamburger
-  });
+  const img = document.createElement("img");
+  img.src = ogImage;
+  img.classList.add("show");
 
-  // Close mobile menu when a link is clicked (for single-page navigation)
-  navLinks.querySelectorAll("a").forEach((link) => {
-    link.addEventListener("click", () => {
-      if (navLinks.classList.contains("active")) {
-        navLinks.classList.remove("active");
-        mobileMenu.classList.remove("active");
-      }
-    });
-  });
+  const LightBoxModal = document.getElementById("LightBox-Modal");
+  const modalContent = document.getElementById("lightbox-modal-content");
+  modalContent.appendChild(img);
 
-  // Add sticky class to navbar on scroll
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 50) {
-      // Adjust threshold as needed
-      navBar.classList.add("scrolled");
-    } else {
-      navBar.classList.remove("scrolled");
-    }
-  });
+  const LightBox = document.getElementById("LightBox-Modal");
+  LightBox.classList.add("show");
+}
 
-  // Optional: Smooth scroll for older browsers or more control
-  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
-
-      const targetId = this.getAttribute("href");
-      const targetElement = document.querySelector(targetId);
-
-      if (targetElement) {
-        // Get the height of the fixed navbar
-        const navbarHeight = navBar.offsetHeight;
-        // Calculate the position, accounting for the navbar
-        const offsetPosition = targetElement.offsetTop - navbarHeight - 20; // -20px for a bit of padding
-
-        window.scrollTo({
-          top: offsetPosition,
-          behavior: "smooth",
-        });
-      }
-    });
-  });
-});
+function closeModal() {
+  const LightBoxModal = document.getElementById("LightBox-Modal");
+  const modalContent = document.getElementById("lightbox-modal-content");
+  modalContent.innerHTML = "";
+  LightBoxModal.classList.remove("show");
+}
